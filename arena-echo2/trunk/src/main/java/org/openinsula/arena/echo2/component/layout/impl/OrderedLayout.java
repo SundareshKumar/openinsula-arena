6b2b5821d94ad11b16c7929d7640a937e9113a73
nobody <nobody@4e3340f0-7962-11de-bdc7-1f637afd43b1>
2008-02-21 23:04:15 +0000

    Alterando a versão para 0.1.2 e nova implementação do OrderedLayout.

--- arena-echo2/trunk/src/main/java/org/openinsula/arena/echo2/component/layout/impl/OrderedLayout.java
+++ arena-echo2/trunk/src/main/java/org/openinsula/arena/echo2/component/layout/impl/OrderedLayout.java
@@ -4,14 +4,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import nextapp.echo2.app.Column;
+import nextapp.echo2.app.Component;
+import nextapp.echo2.app.Extent;
+import nextapp.echo2.app.Label;
+import nextapp.echo2.app.Row;
+
 import org.openinsula.arena.echo2.component.div.Div;
 import org.openinsula.arena.echo2.component.layout.AbstractLayout;
 import org.openinsula.arena.echo2.component.layout.Layout;
-
-import nextapp.echo2.app.Column;
-import nextapp.echo2.app.Component;
-import nextapp.echo2.app.Grid;
-import nextapp.echo2.app.Label;
 
 public class OrderedLayout extends AbstractLayout {
 
@@ -29,6 +30,8 @@ public class OrderedLayout extends AbstractLayout {
 	
 	private boolean captionVisibility = true;
 	
+	private int captionWidth = -1;
+	
 	public OrderedLayout() {
 	}
 	
@@ -37,31 +40,14 @@ public class OrderedLayout extends AbstractLayout {
 		this.orientation = orientation;
 	}
 
-	public Component buildLayout() {
+	/**
+	 * @return
+	 */
+	public Component buildFormLayout() {
 		List<LayoutEntry> properties = getProperties();
 		
-		Component main = null;
-		
-		int lineNumber = 2;
-		
-		if (!isCaptionVisibility()) {
-			lineNumber = 1;
-		}
-		
-		switch (orientation) {
-			case ORIENTATION_FORM:
-				main = new Column();
-				break;
-			case ORIENTATION_HORIZONTAL: 
-				main = new Grid(lineNumber);
-				((Grid)main).setOrientation(Grid.ORIENTATION_HORIZONTAL);
-				break;
-			case ORIENTATION_VERTICAL:
-				main = new Grid(lineNumber);
-				((Grid)main).setOrientation(Grid.ORIENTATION_VERTICAL);
-				break;
-		}
-		
+		Component main = new Column();
+
 		for (LayoutEntry entry : properties) {
 			switch (entry.getLayoutPropertyType()) {
 			case LAYOUT:
@@ -106,10 +92,147 @@ public class OrderedLayout extends AbstractLayout {
 				break;
 			}
 		}
-		
 		return main;
 	}
+	
+	/**
+	 * @return
+	 */
+	public Component buildHorizontalLayout() {
+		List<LayoutEntry> properties = getProperties();
+		
+		Component main = new Column();
 
+		for (LayoutEntry entry : properties) {
+			switch (entry.getLayoutPropertyType()) {
+			case LAYOUT:
+				Layout layout = (Layout) entry.getLayoutElement();
+				Component buildLayout = layout.buildLayout();
+				
+				main.add(buildAndConfigureDiv(buildLayout));
+				break;
+			case COMPONENT:
+				LayoutComponent layoutComponent = (LayoutComponent) entry.getLayoutElement();
+				
+				Row row = new Row();
+				
+				if (layoutComponent.getCaption() != null) {
+					Div buildAndConfigureDiv = buildAndConfigureDiv(layoutComponent.getCaption());
+					if (getCaptionWidth() != -1) {
+						buildAndConfigureDiv.setWidth(new Extent(getCaptionWidth()));
+					}
+					row.add(buildAndConfigureDiv);
+				} else {
+					Div buildAndConfigureDiv = buildAndConfigureDiv(new Label(layoutComponent.getField().getId()));
+					if (getCaptionWidth() != -1) {
+						buildAndConfigureDiv.setWidth(new Extent(getCaptionWidth()));
+					}
+					row.add(buildAndConfigureDiv);
+				}
+				configureComponent(layoutComponent.getCaption());
+				configureComponent(layoutComponent.getField());
+				
+				row.add(buildAndConfigureDiv(layoutComponent.getField()));
+				
+				main.add(row);
+				break;
+			case STYLE:
+				LayoutStyle layoutStyle = (LayoutStyle) entry.getLayoutElement();
+				
+				if (layoutStyle.getKlazz() == null) {
+					if (divStyles.contains(layoutStyle)) {
+						if (!divStyles.remove(layoutStyle)) {
+							logger.warn("Error on deleting from divStyles list, the property: "+layoutStyle.getName());
+						}
+					}
+					
+					divStyles.add(layoutStyle);
+				} else {
+					if (componentStyles.contains(layoutStyle)) {
+						if (!componentStyles.remove(layoutStyle)) {
+							logger.warn("Error on deleting from componentStyles list, the property: "+layoutStyle.getName());
+						}
+					}
+					
+					componentStyles.add(layoutStyle);
+				}
+				break;
+			}
+		}
+		return main;
+	}
+	
+	/**
+	 * @return
+	 */
+	public Component buildVerticalLayout() {
+		List<LayoutEntry> properties = getProperties();
+		
+		Row main = new Row();
+
+		for (LayoutEntry entry : properties) {
+			switch (entry.getLayoutPropertyType()) {
+			case LAYOUT:
+				Layout layout = (Layout) entry.getLayoutElement();
+				Component buildLayout = layout.buildLayout();
+				
+				main.add(buildAndConfigureDiv(buildLayout));
+				break;
+			case COMPONENT:
+				LayoutComponent layoutComponent = (LayoutComponent) entry.getLayoutElement();
+				
+				Column column = new Column();
+				
+				if (layoutComponent.getCaption() != null) {
+					column.add(buildAndConfigureDiv(layoutComponent.getCaption()));
+				} else {
+					column.add(buildAndConfigureDiv(new Label(layoutComponent.getField().getId())));
+				}
+				configureComponent(layoutComponent.getCaption());
+				configureComponent(layoutComponent.getField());
+				
+				column.add(buildAndConfigureDiv(layoutComponent.getField()));
+				
+				main.add(column);
+				break;
+			case STYLE:
+				LayoutStyle layoutStyle = (LayoutStyle) entry.getLayoutElement();
+				
+				if (layoutStyle.getKlazz() == null) {
+					if (divStyles.contains(layoutStyle)) {
+						if (!divStyles.remove(layoutStyle)) {
+							logger.warn("Error on deleting from divStyles list, the property: "+layoutStyle.getName());
+						}
+					}
+					
+					divStyles.add(layoutStyle);
+				} else {
+					if (componentStyles.contains(layoutStyle)) {
+						if (!componentStyles.remove(layoutStyle)) {
+							logger.warn("Error on deleting from componentStyles list, the property: "+layoutStyle.getName());
+						}
+					}
+					
+					componentStyles.add(layoutStyle);
+				}
+				break;
+			}
+		}
+		return main;
+	}
+		
+	public Component buildLayout() {
+		switch (orientation) {
+		case ORIENTATION_FORM:
+			return buildFormLayout();
+		case ORIENTATION_HORIZONTAL: 
+			return buildHorizontalLayout(); 
+		case ORIENTATION_VERTICAL:
+			return buildVerticalLayout();
+		}
+		return null;
+	}
+	
 	private void configureComponent(Component field) {
 		if (field == null) {
 			return;
@@ -143,6 +266,14 @@ public class OrderedLayout extends AbstractLayout {
 
 	public void setCaptionVisibility(boolean captionVisibility) {
 		this.captionVisibility = captionVisibility;
+	}
+
+	public int getCaptionWidth() {
+		return captionWidth;
+	}
+
+	public void setCaptionWidth(int captionWidth) {
+		this.captionWidth = captionWidth;
 	}
 	
 }
