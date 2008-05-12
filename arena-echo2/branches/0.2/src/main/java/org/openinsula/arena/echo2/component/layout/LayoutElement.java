773ab49a043bafcf7ed380657091e1319c631787
realm <realm@4e3340f0-7962-11de-bdc7-1f637afd43b1>
2008-05-12 13:53:37 +0000

    Versão do branches 0.2 eliminada.

--- arena-echo2/branches/0.2/src/main/java/org/openinsula/arena/echo2/component/layout/LayoutElement.java
+++
@@ -1,5 +0,0 @@
-package org.openinsula.arena.echo2.component.layout;
-
-public interface LayoutElement {
-
-}
--- arena-echo2/branches/0.2/src/main/java/org/openinsula/arena/echo2/component/model/SortableTableModel.java
+++
@@ -1,9 +0,0 @@
-package org.openinsula.arena.echo2.component.model;
-
-import nextapp.echo2.app.table.TableModel;
-
-public interface SortableTableModel extends TableModel {
-	public abstract boolean isSortable(int column);
-	
-	public abstract void sortTable(int column, boolean order);
-}
