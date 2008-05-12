136ebeded0d33be38d0736a8e71e8b707291f7c4
realm <realm@4e3340f0-7962-11de-bdc7-1f637afd43b1>
2008-05-12 17:24:14 +0000

    Inserindo componentes de Download e Upload.

---
+++ arena-echo2/branches/0.1/src/main/java/org/openinsula/arena/echo2/component/provider/PdfDownloadProvider.java
@@ -0,0 +1,27 @@
+package org.openinsula.arena.echo2.component.provider;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+public class PdfDownloadProvider extends AbstractDownloadProvider {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param jasperPrint
+	 * @throws JRException
+	 */
+	public PdfDownloadProvider(JasperPrint jasperPrint) throws JRException {
+		initDownloadProvider(JasperExportManager.exportReportToPdf(jasperPrint));
+	}
+
+	public PdfDownloadProvider(byte[] byteArray) {
+		initDownloadProvider(byteArray);
+	}
+
+	private void initDownloadProvider(byte[] byteArray) {
+		setByteArray(byteArray);
+		setContentType("application/pdf");
+		setFileName("relatorio.pdf");
+	}
+}
