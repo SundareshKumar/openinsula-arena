35b0f0397b2a0ddc1933d7c95d8fdc493a2d8039
realm <realm@4e3340f0-7962-11de-bdc7-1f637afd43b1>
2008-05-12 14:12:30 +0000

    Criando o branch 0.1 a partir da tag 0.1.2.

---
+++ arena-echo2/branches/0.1/src/main/java/org/openinsula/arena/echo2/component/util/ComponentSize.java
@@ -0,0 +1,45 @@
+package org.openinsula.arena.echo2.component.util;
+
+/**
+ * Enum responsável por definir o tamanho dos componentes (px).
+ * 
+ * @author utiumi
+ */
+public enum ComponentSize {
+
+	/** Tamanho 1 (30px). */
+	SIZE_1(30),
+
+	/** Tamanho 2 (80px). */
+	SIZE_2(80),
+
+	/** Tamanho 3 (120px). */
+	SIZE_3(120),
+
+	/** Tamanho 4 (220px). */
+	SIZE_4(220),
+
+	/** Tamanho 5 (320px). */
+	SIZE_5(320);
+
+	/** Atributo que guarda o tamanho do componente. */
+	private Integer size;
+
+	/**
+	 * Constrói um novo <code>ComponentSize</code>.
+	 * 
+	 * @param paramSize tamanho do componente.
+	 */
+	private ComponentSize(final Integer paramSize) {
+		size = paramSize;
+	}
+
+	/**
+	 * Getter para o atributo <code>size</code>.
+	 * 
+	 * @return tamanho do componente.
+	 */
+	public Integer getSize() {
+		return size;
+	}
+}
