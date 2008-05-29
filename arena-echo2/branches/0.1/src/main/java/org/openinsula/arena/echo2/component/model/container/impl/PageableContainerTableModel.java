9ded55aedea5bdd0aa988698a0bf79b927fe5b3f
realm <realm@4e3340f0-7962-11de-bdc7-1f637afd43b1>
2008-05-29 11:09:01 +0000

    Fazendo um merge do trunk para corrigir o uso da PageableContainerTableModel.

--- arena-echo2/branches/0.1/src/main/java/org/openinsula/arena/echo2/component/model/container/impl/PageableContainerTableModel.java
+++ arena-echo2/branches/0.1/src/main/java/org/openinsula/arena/echo2/component/model/container/impl/PageableContainerTableModel.java
@@ -4,36 +4,58 @@ import org.openinsula.arena.echo2.component.model.PageableTableModel;
 
 public class PageableContainerTableModel<T> extends BeanReflectionContainerTableModel<T> implements PageableTableModel {
 	private static final long serialVersionUID = 1L;
-	
+
 	public PageableContainerTableModel() {
 	}
-	
+
 	public PageableContainerTableModel(boolean permitDuplicates, boolean substituteDuplicate) {
 		super(permitDuplicates, substituteDuplicate);
 	}
 
 	private int currentPage;
-	
+
 	private int pageSize = 20;
-	
+
+	@Override
+	public Object getValueAt(int columnIndex, int rowIndex) {
+		rowIndex += (getPageSize() * getCurrentPage());
+		return super.getValueAt(columnIndex, rowIndex);
+	}
+
+	@Override
+	public int getRowCount() {
+		return getCurrentPageSize();
+	}
+
+	private int getCurrentPageSize() {
+		int currentPageSize = getPageSize();
+
+		if ((getPageCount() - 1) == getCurrentPage()) {
+			currentPageSize = size() % this.pageSize;
+		}
+
+		return currentPageSize;
+	}
+
 	public int getCurrentPage() {
 		return currentPage;
 	}
 
 	public int getPageCount() {
 		int size = size();
-		
+
 		int pageCount = size / pageSize;
-		
+
 		if ((size % pageSize) > 0) {
 			pageCount++;
 		}
-		
+
 		return pageCount;
 	}
 
 	public void setCurrentPage(int index) {
 		this.currentPage = index;
+		this.fireTableDataChanged();
 	}
 
 	public void setPageSize(int index) {
