567bd1822cbaa455d71d38a401b90c0c8c53d8f8
fabiano <fabiano@4e3340f0-7962-11de-bdc7-1f637afd43b1>
2008-06-17 14:28:24 +0000

    - adicionada DataTable e DataTableModel para trabalhar com tabelas no gwt. - criado tema padrao para o novo componente

--- arena-gwt/branches/1.1/src/main/java/org/openinsula/arena/gwt/client/Main.java
+++ arena-gwt/branches/1.1/src/main/java/org/openinsula/arena/gwt/client/Main.java
@@ -1,9 +1,41 @@
 package org.openinsula.arena.gwt.client;
 
+import java.util.Arrays;
+
+import org.openinsula.arena.gwt.client.components.datatable.AbstractDataTableModel;
+import org.openinsula.arena.gwt.client.components.datatable.DataTable;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class Main implements EntryPoint {
 
 	public void onModuleLoad() {
+		
+		DataTable<String> table = new DataTable<String>();
+		table.setModel(new AbstractDataTableModel<String>() {
+			private final String[] columns = new String[] {"Valor", "Tamanho"};
+			@Override
+			protected String[] getColumnNames() {
+				return columns;
+			}
+
+			@Override
+			protected Object getValueAtColumn(String bean, int column) {
+				switch (column) {
+				case 0:
+					return bean;
+				case 1: 
+					return bean.length();
+				}
+				return null;
+			}
+		});
+		table.setVisibleRows(4);
+		table.getModel().setItens(Arrays.asList("teste", "123", "maeda", "insula", "java", "maringa", "googlewebtoolkit", "php", "pascal",
+				"ruby", "phyton", "smalltalk", "c++", "blues", "rock", "jazz"));
+		
+		RootPanel.get().add(table);
+		
 	}
 }
