3eed0a328e19eed4a6f52fc84cef475df83adf8a
erdanielli <erdanielli@4e3340f0-7962-11de-bdc7-1f637afd43b1>
2008-10-29 19:46:49 +0000

    + FIXED index.html renamed to _HTTP.html to avoid conflict

--- arena-gwt/branches/1.2m1/src/main/java/org/openinsula/arena/gwt/http/client/Main.java
+++ arena-gwt/branches/1.2m1/src/main/java/org/openinsula/arena/gwt/http/client/Main.java
@@ -1,13 +1,10 @@
 package org.openinsula.arena.gwt.http.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
 
 public class Main implements EntryPoint {
 
 	public void onModuleLoad() {
-		RootPanel.get().add(new Label("OK"));
 	}
 
 }
