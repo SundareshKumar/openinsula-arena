1c2229ee8d694211234c148ad9ba1ba10abf7b89
anderson <anderson@4e3340f0-7962-11de-bdc7-1f637afd43b1>
2009-03-09 19:44:10 +0000

    SALTO-81

--- arena-gwt/branches/1.2/src/main/java/org/openinsula/arena/gwt/client/ui/form/FormFactory.java
+++ arena-gwt/branches/1.2/src/main/java/org/openinsula/arena/gwt/client/ui/form/FormFactory.java
@@ -32,7 +32,7 @@ public abstract class FormFactory {
 		styleBuilder = new StyleBuilder();
 	}
 
-	public static void setModulePrefix(String modulePrefix) {
+	public static void setModulePrefix(final String modulePrefix) {
 		styleBuilder.setModulePrefix(modulePrefix);
 	}
 
@@ -84,43 +84,43 @@ public abstract class FormFactory {
 
 	static final String STYLE_FORM_TEXT_BOX_DISABLED = "FormItemTextBoxDisabled";
 
-	public static Label label(String text) {
-		Label label = new Label(text);
+	public static Label label(final String text) {
+		final Label label = new Label(text);
 		label.setStyleName(styleBuilder.getRule(STYLE_FORM_LABEL));
 		return label;
 	}
 
-	public static Label labelH1(String text) {
-		Label label = new Label(text);
+	public static Label labelH1(final String text) {
+		final Label label = new Label(text);
 		label.setStyleName(styleBuilder.getRule(STYLE_FORM_LABEL_TITULO_H1));
 		return label;
 	}
 
 	public static Label optionalLabel() {
-		Label label = new Label("(opcional)");
+		final Label label = new Label("(opcional)");
 		label.setStyleName(styleBuilder.getRule(STYLE_FORM_LABEL_OPTIONAL));
 		return label;
 	}
 
-	public static Label suffixLabel(String text) {
-		Label label = new Label(text);
+	public static Label suffixLabel(final String text) {
+		final Label label = new Label(text);
 		label.setStyleName(styleBuilder.getRule(STYLE_FORM_LABEL_SUFFIX));
 		return label;
 	}
 
-	public static Label errorLabel(String text) {
-		Label label = new Label(text);
+	public static Label errorLabel(final String text) {
+		final Label label = new Label(text);
 		label.setStyleName(styleBuilder.getRule(STYLE_FORM_LABEL_ERROR));
 		return label;
 	}
 
 	public static TextBox textBox() {
-		TextBox textBox = new TextBox();
+		final TextBox textBox = new TextBox();
 		return textBox;
 	}
 
-	public static TextBox textBox(String width) {
-		TextBox textBox = new TextBox();
+	public static TextBox textBox(final String width) {
+		final TextBox textBox = new TextBox();
 		textBox.setWidth(width);
 		return textBox;
 	}
@@ -140,42 +140,42 @@ public abstract class FormFactory {
 	 */
 	@Deprecated
 	public static TextBox textBoxDate() {
-		TextBox box = textBox("80px");
+		final TextBox box = textBox("80px");
 		box.setMaxLength(10);
 		return box;
 	}
 
-	public static Button button(String text) {
-		Button button = new Button(text);
+	public static Button button(final String text) {
+		final Button button = new Button(text);
 		return button;
 	}
 
 	public static VerticalPanel verticalPanel() {
-		VerticalPanel panel = new VerticalPanel();
+		final VerticalPanel panel = new VerticalPanel();
 		panel.setStyleName(styleBuilder.getRule(STYLE_FORM_VERTICAL_PANEL));
 		return panel;
 	}
 
 	public static PasswordTextBox passwordTextBox() {
-		PasswordTextBox textBox = new PasswordTextBox();
+		final PasswordTextBox textBox = new PasswordTextBox();
 		return textBox;
 	}
 
 	public static PopupPanel hintPopupPanel() {
-		PopupPanel panel = new PopupPanel(true);
+		final PopupPanel panel = new PopupPanel(true);
 		panel.setStyleName(styleBuilder.getRule(STYLE_FORM_PANEL_HINT));
 		return panel;
 	}
 
 	public static TextBox disabledTextBox() {
-		TextBox textBox = new TextBox();
+		final TextBox textBox = new TextBox();
 		textBox.setStyleName(styleBuilder.getRule(STYLE_FORM_TEXT_BOX_DISABLED));
 		textBox.setEnabled(false);
 		return textBox;
 	}
 
-	public static TextArea textArea(String width, String height) {
-		TextArea textArea = new TextArea();
+	public static TextArea textArea(final String width, final String height) {
+		final TextArea textArea = new TextArea();
 		textArea.setSize(width, height);
 		return textArea;
 	}
@@ -185,7 +185,7 @@ public abstract class FormFactory {
 	}
 
 	public static TextArea disabledTextArea() {
-		TextArea textArea = textArea();
+		final TextArea textArea = textArea();
 		textArea.setEnabled(false);
 		return textArea;
 	}
@@ -198,8 +198,8 @@ public abstract class FormFactory {
 	 * @param label
 	 * @return
 	 */
-	public static Hyperlink hyperlink(String label) {
-		if (label == null || label.trim().isEmpty()) {
+	public static Hyperlink hyperlink(final String label) {
+		if (label == null || label.trim().equals("")) {
 			return hyperlink("no label");
 		}
 
@@ -211,36 +211,36 @@ public abstract class FormFactory {
 		return hyperlink(label, noTokenAtAll);
 	}
 
-	public static Hyperlink hyperlink(String label, String targetHistoryToken) {
+	public static Hyperlink hyperlink(final String label, final String targetHistoryToken) {
 		return new Hyperlink(label, targetHistoryToken);
 	}
 
 	public static HorizontalPanel horizontalPanel() {
-		HorizontalPanel horizontalPanel = new HorizontalPanel();
+		final HorizontalPanel horizontalPanel = new HorizontalPanel();
 		horizontalPanel.setSpacing(5);
 		return horizontalPanel;
 	}
 
 	public static HorizontalPanel actionPanel() {
-		Widget[] ws = new Widget[] {};
+		final Widget[] ws = new Widget[] {};
 		return actionPanel(ws);
 	}
 
-	public static HorizontalPanel actionPanel(Widget... widgets) {
-		HorizontalPanel panel = new HorizontalPanel();
+	public static HorizontalPanel actionPanel(final Widget... widgets) {
+		final HorizontalPanel panel = new HorizontalPanel();
 		panel.setVerticalAlignment(HasVerticalAlignment.ALIGN_BOTTOM);
 
-		for (Widget widget : widgets) {
+		for (final Widget widget : widgets) {
 			panel.add(widget);
 		}
 
 		return panel;
 	}
 
-	public static Panel formBuilderToPanel(FormItem<?>... formItems) {
-		FormBuilder formBuilder = new FormBuilder();
+	public static Panel formBuilderToPanel(final FormItem<?>... formItems) {
+		final FormBuilder formBuilder = new FormBuilder();
 
-		for (FormItem<?> formItem : formItems) {
+		for (final FormItem<?> formItem : formItems) {
 			formBuilder.add(formItem);
 		}
 
@@ -251,11 +251,11 @@ public abstract class FormFactory {
 		return dateFormItem("Data");
 	}
 
-	public static DateFormItem dateFormItem(String label) {
+	public static DateFormItem dateFormItem(final String label) {
 		return dateFormItem(label, null, false, true);
 	}
 
-	public static DateFormItem dateFormItem(String label, String hint, boolean optional, boolean hasLabels) {
+	public static DateFormItem dateFormItem(final String label, final String hint, final boolean optional, final boolean hasLabels) {
 		SimpleDateWidget widget;
 
 		if (hasLabels) {
@@ -265,7 +265,7 @@ public abstract class FormFactory {
 			widget = new SimpleDateWidget();
 		}
 
-		DateFormItem formItem = new DateFormItem(label, widget, hint, optional);
+		final DateFormItem formItem = new DateFormItem(label, widget, hint, optional);
 
 		if (!optional) {
 			formItem.addFormItemValidator(new DateFormItemValidator());
@@ -274,7 +274,7 @@ public abstract class FormFactory {
 		return formItem;
 	}
 
-	public static SimpleDateWidget simpleDateWidget(String ano, String mes, String dia) {
+	public static SimpleDateWidget simpleDateWidget(final String ano, final String mes, final String dia) {
 		final SimpleDateWidget simpleDateWidget = new SimpleDateWidget(LabelAlignment.LEFT, "ano", "m\u00eas", "dia");
 		simpleDateWidget.setStyleName(styleBuilder.getRule(STYLE_FORM_SIMPLE_DATE_WIDGET));
 		return simpleDateWidget;
@@ -284,32 +284,32 @@ public abstract class FormFactory {
 		return simpleDateWidget("ano", "m\u00eas", "dia");
 	}
 
-	public static FormItem<TextBox> formItemTextBox(String label) {
+	public static FormItem<TextBox> formItemTextBox(final String label) {
 		return formItemTextBox(label, false);
 	}
 
-	public static FormItem<TextBox> formItemTextBox(String label, boolean mandatory) {
+	public static FormItem<TextBox> formItemTextBox(final String label, final boolean mandatory) {
 		return formItemTextBox(label, FormFactory.textBox(), mandatory);
 	}
 
-	public static FormItem<TextBox> formItemTextBoxSmall(String label) {
+	public static FormItem<TextBox> formItemTextBoxSmall(final String label) {
 		return formItemTextBoxSmall(label, false);
 	}
 
-	public static FormItem<TextBox> formItemTextBoxSmall(String label, boolean mandatory) {
+	public static FormItem<TextBox> formItemTextBoxSmall(final String label, final boolean mandatory) {
 		return formItemTextBox(label, FormFactory.textBoxSmall(), mandatory);
 	}
 
-	public static FormItem<TextBox> formItemTextBoxBig(String label) {
+	public static FormItem<TextBox> formItemTextBoxBig(final String label) {
 		return formItemTextBoxBig(label, false);
 	}
 
-	public static FormItem<TextBox> formItemTextBoxBig(String label, boolean mandatory) {
+	public static FormItem<TextBox> formItemTextBoxBig(final String label, final boolean mandatory) {
 		return formItemTextBox(label, FormFactory.textBoxBig(), mandatory);
 	}
 
-	private static FormItem<TextBox> formItemTextBox(String label, TextBox widget, boolean mandatory) {
-		FormItem<TextBox> formItem = new FormItem<TextBox>(label, widget);
+	private static FormItem<TextBox> formItemTextBox(final String label, final TextBox widget, final boolean mandatory) {
+		final FormItem<TextBox> formItem = new FormItem<TextBox>(label, widget);
 
 		if (mandatory) {
 			formItem.addFormItemValidator(new NotNullFormItemValidator());
