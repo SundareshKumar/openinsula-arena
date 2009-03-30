31f3fc526217af83dfc43c6f677ed20812068b2c
erdanielli <erdanielli@4e3340f0-7962-11de-bdc7-1f637afd43b1>
2009-03-30 20:11:37 +0000

    + FocusUtils aprimorado

--- arena-gwt/trunk/arena-gwt/src/main/java/org/openinsula/arena/gwt/util/client/FocusUtils.java
+++ arena-gwt/trunk/arena-gwt/src/main/java/org/openinsula/arena/gwt/util/client/FocusUtils.java
@@ -2,7 +2,10 @@ package org.openinsula.arena.gwt.util.client;
 
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.ui.FocusListenerAdapter;
 import com.google.gwt.user.client.ui.HasFocus;
+import com.google.gwt.user.client.ui.TextBoxBase;
+import com.google.gwt.user.client.ui.Widget;
 
 public abstract class FocusUtils {
 
@@ -14,6 +17,15 @@ public abstract class FocusUtils {
 				}
 			});
 		}
+	}
+	
+	public static void selectAllOnFocus(final TextBoxBase target) {
+		target.addFocusListener(new FocusListenerAdapter() {
+			public void onFocus(final Widget sender) {
+				target.setCursorPos(target.getText().length());
+				target.selectAll();
+			}
+		});
 	}
 
 }
