f3bc0bed5d2e43ec7fcd8fabd58b7ee8b724d7d1
yanaga <yanaga@4e3340f0-7962-11de-bdc7-1f637afd43b1>
2009-06-04 20:37:00 +0000

    Added new CNAB240 test with a SAP-generated file.

--- arena-io/trunk/src/test/java/org/openinsula/arena/io/textfile/impl/cnab240/Cnab240FileParserFactoryTestCase.java
+++ arena-io/trunk/src/test/java/org/openinsula/arena/io/textfile/impl/cnab240/Cnab240FileParserFactoryTestCase.java
@@ -22,7 +22,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.openinsula.arena.io.textfile.FileParser;
 import org.openinsula.arena.io.textfile.FileParserFactory;
@@ -30,69 +29,70 @@ import org.openinsula.arena.io.textfile.line.Line;
 import org.openinsula.arena.io.textfile.parser.LineParser;
 
 public class Cnab240FileParserFactoryTestCase {
-	private int headerArquivoCount = 0;
 
-	private int headerLoteCount = 0;
+	@Test
+	public void testNewCnab240RemessaFileParser() throws IOException {
+		Cnab240Handler handler = new Cnab240Handler();
+		FileParser fileParser = FileParserFactory.newCnab240RemessaFileParser();
+		fileParser.parse(getClass().getResourceAsStream("/cnab240/remessa1.txt"), handler);
 
-	private int segmentoPCount = 0;
-
-	private int segmentoQCount = 0;
-
-	private int segmentoRCount = 0;
-
-	private int segmentoTCount = 0;
-
-	private int segmentoUCount = 0;
-
-	private int trailerLoteCount = 0;
-
-	private int trailerArquivoCount = 0;
-
-	@Before
-	public void initCounter() {
-		headerArquivoCount = 0;
-		headerLoteCount = 0;
-		segmentoPCount = 0;
-		segmentoQCount = 0;
-		segmentoRCount = 0;
-		segmentoTCount = 0;
-		segmentoUCount = 0;
-		trailerLoteCount = 0;
-		trailerArquivoCount = 0;
+		assertEquals(1, handler.headerArquivoCount);
+		assertEquals(1, handler.headerLoteCount);
+		assertEquals(1, handler.segmentoPCount);
+		assertEquals(1, handler.segmentoQCount);
+		assertEquals(1, handler.segmentoRCount);
+		assertEquals(1, handler.trailerLoteCount);
+		assertEquals(1, handler.trailerLoteCount);
 	}
 
 	@Test
-	@SuppressWarnings("unused")
-	public void testNewCnab240RemessaFileParser() throws IOException {
-		initCounter();
+	public void testNewCnab240RemessaFileParser2() throws IOException {
+		Cnab240Handler handler = new Cnab240Handler();
 		FileParser fileParser = FileParserFactory.newCnab240RemessaFileParser();
-		fileParser.parse(getClass().getResourceAsStream("/cnab240/remessa1.txt"), new Cnab240Handler());
+		fileParser.parse(getClass().getResourceAsStream("/cnab240/remessa2.txt"), handler);
 
-		assertEquals(1, headerArquivoCount);
-		assertEquals(1, headerLoteCount);
-		assertEquals(1, segmentoPCount);
-		assertEquals(1, segmentoQCount);
-		assertEquals(1, segmentoRCount);
-		assertEquals(1, trailerLoteCount);
-		assertEquals(1, trailerLoteCount);
+		assertEquals(1, handler.headerArquivoCount);
+		assertEquals(1, handler.headerLoteCount);
+		assertEquals(1, handler.segmentoPCount);
+		assertEquals(1, handler.segmentoQCount);
+		assertEquals(1, handler.trailerLoteCount);
+		assertEquals(1, handler.trailerLoteCount);
 	}
 
 	@Test
 	public void testNewCnab240RetornoFileParser() throws IOException {
-		initCounter();
+		Cnab240Handler handler = new Cnab240Handler();
 		FileParser fileParser = FileParserFactory.newCnab240RetornoFileParser();
-		fileParser.parse(getClass().getResourceAsStream("/cnab240/retorno1.txt"), new Cnab240Handler());
+		fileParser.parse(getClass().getResourceAsStream("/cnab240/retorno1.txt"), handler);
 
-		assertEquals(1, headerArquivoCount);
-		assertEquals(1, headerLoteCount);
-		assertEquals(18, segmentoTCount);
-		assertEquals(18, segmentoUCount);
-		assertEquals(1, trailerLoteCount);
-		assertEquals(1, trailerLoteCount);
+		assertEquals(1, handler.headerArquivoCount);
+		assertEquals(1, handler.headerLoteCount);
+		assertEquals(18, handler.segmentoTCount);
+		assertEquals(18, handler.segmentoUCount);
+		assertEquals(1, handler.trailerLoteCount);
+		assertEquals(1, handler.trailerLoteCount);
 
 	}
 
 	public class Cnab240Handler {
+		private int headerArquivoCount = 0;
+
+		private int headerLoteCount = 0;
+
+		private int segmentoPCount = 0;
+
+		private int segmentoQCount = 0;
+
+		private int segmentoRCount = 0;
+
+		private int segmentoTCount = 0;
+
+		private int segmentoUCount = 0;
+
+		private int trailerLoteCount = 0;
+
+		private int trailerArquivoCount = 0;
+
 		@LineParser(HeaderArquivoLineFactory.class)
 		public void headerArquivo(Line line) {
 			headerArquivoCount++;
